b986ed804d70f13a3abec4de5c3c7f2bcfe9211a
Gemma <gemma.percival@hotmail.co.uk>
2018-07-30 16:53:45 +0100

    Planets java lab

--- src/main/java/Planet.java
+++ src/main/java/Planet.java
@@ -29,31 +29,3 @@ public class Planet {
     }
 
 }
-
-
-
-
-//class Planet
-
-//  def initialize(name, size)
-//@name = name
-//@size = size
-//        end
-//
-//        def get_name()
-//        return @name
-//  end
-//
-//          def get_size()
-//          return @size
-//  end
-//
-//          def explode()
-//          puts "Boom! #{@name} has exploded."
-//          end
-//
-//          end
-//
-//          mars = Planet.new("Mars", 908973)
-//          puts "#{mars.get_name()} is #{mars.get_size()}"
-//          mars.explode()
